cde9add7a5fe335a2cd43132685e8af98b79550b
arwaUnox <arwa.fahoud@unox.com>
2025-04-04 19:14:05 +0200

    add client instance with together api

--- src/llm_utils.py
+++ src/llm_utils.py
@@ -3,11 +3,15 @@ import os
 
 # client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-openai.api_key = 'b5f2f47668eba0617301fda1650c426327dbeed73a928ae36fe5f3e8c2ac3bd5'
-openai.api_base =  "https://api.together.xyz/v1"
+# Create the client instance with Together.ai's endpoint
+client = OpenAI(
+    api_key="b5f2f47668eba0617301fda1650c426327dbeed73a928ae36fe5f3e8c2ac3bd5",
+    base_url="https://api.together.xyz/v1"
+)
+
 
 def generate_code_review(prompt):
-    response =  openai.ChatCompletion.create(
+    response = client.chat.completions.create(
         # model="gpt-3.5-turbo",
         model="mistralai/Mixtral-8x7B-Instruct-v0.1",
         messages=[
