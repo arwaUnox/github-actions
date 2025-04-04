ebc0b76114d518ca32a105fc8172707a4e61275d
arwaUnox <arwa.fahoud@unox.com>
2025-04-04 19:10:13 +0200

    add together instead

--- src/llm_utils.py
+++ src/llm_utils.py
@@ -1,11 +1,15 @@
 import openai
 import os
 
-client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+# client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+openai.api_key = 'b5f2f47668eba0617301fda1650c426327dbeed73a928ae36fe5f3e8c2ac3bd5'
+openai.api_base =  "https://api.together.xyz/v1"
 
 def generate_code_review(prompt):
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        # model="gpt-3.5-turbo",
+        model="mistralai/Mixtral-8x7B-Instruct-v0.1",
         messages=[
             {
                 "role": "system",
