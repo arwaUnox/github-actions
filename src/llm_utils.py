c53925900b2498f5edf98ffda6de62142b888e86
arwaUnox <arwa.fahoud@unox.com>
2025-04-04 19:11:27 +0200

    fix

--- src/llm_utils.py
+++ src/llm_utils.py
@@ -7,7 +7,7 @@ openai.api_key = 'b5f2f47668eba0617301fda1650c426327dbeed73a928ae36fe5f3e8c2ac3b
 openai.api_base =  "https://api.together.xyz/v1"
 
 def generate_code_review(prompt):
-    response = client.chat.completions.create(
+    response =  openai.ChatCompletion.create(
         # model="gpt-3.5-turbo",
         model="mistralai/Mixtral-8x7B-Instruct-v0.1",
         messages=[
